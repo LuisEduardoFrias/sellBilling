8a955b3aace5527980d7717d8e280fefba868fef
Luis Eduardo Frias <luiseduardofrias27@email.com>
2023-09-13 02:03:57 -0400

    trabajando con el componente plugins

--- src/components/dashboard/DashBoard.tsx
+++ src/components/dashboard/DashBoard.tsx
@@ -1,4 +1,5 @@
-import { lazy, CSSProperties } from "react";
+import { lazy, CSSProperties, useEffect, useState } from "react";
+import routes from '../../routes.json';
 
 const Outlet = lazy(()=> import("react-router-dom").then(module => ({ default: module.Outlet})));
 
@@ -19,29 +20,29 @@ interface IDashBoard {
  mainCss?: CSSProperties;
 }
 
-let menus : IOption[] = [
-  {
-    name:"Home",
-    icon:"home",
-    href:"/",
-    isLoad: false,
-    sub: []
-  },
-  {
-    name:"Contact",
-    href: "/contact",
-    icon:"checklist_rtl",
-    isLoad: false,
-    sub: []
-  },
-  {
-    name:"About",
-    icon:"folder_copy",
-    href:"/about",
-    isLoad: false,
-    sub: []
-  }
-]
+// let menus : IOption[] = [
+//   {
+//     name:"Home",
+//     icon:"home",
+//     href:"/",
+//     isLoad: false,
+//     sub: []
+//   },
+//   {
+//     name:"Contact",
+//     href: "/contact",
+//     icon:"checklist_rtl",
+//     isLoad: false,
+//     sub: []
+//   },
+//   {
+//     name:"About",
+//     icon:"folder_copy",
+//     href:"/about",
+//     isLoad: false,
+//     sub: []
+//   }
+// ]
 
 const tools: ITools[] = [
  {icon:"more", isInternal: true, componentName: "Home" },
@@ -60,14 +61,29 @@ const tools: ITools[] = [
 ];
 
 export default function DashBoard(props:IDashBoard) : JSX.Element {
+ 
+ const [menus, setMenus] = useState<IOption[]>([]);
+ 
+ useEffect(() => {
+   routes.map((route: _route) => {
+     setMenus(prevChildren => [...prevChildren,  {
+       name: route.name,
+       icon: route.icon,
+       href: route.path,
+       isLoad: route.isLoad,
+       sub: route.sub
+     }])
+   })
+ }, []);
+ 
   return (
    <div id="layout" style={props.layoutCss} >
     <aside style={props.asideCss} >
      <NavBar menus={menus} />
     </aside>
-    <header style={props.headerCss} > 
-      <a href="#" id="menuIcon" ><Icon>menu</Icon></a>
-      <Header tools={tools} tittle={"EComers"} />
+    <header style={props.headerCss} >
+     <a href="#" id="menuIcon" ><Icon>menu</Icon></a>
+     <Header tools={tools} tittle={"EComers"} />
     </header>
     <main style={props.mainCss} >
      <Outlet />
--- src/plugins/pages/PluginNew.tsx
+++ src/plugins/pages/PluginNew.tsx
@@ -1,8 +1,164 @@
 
+import Img from '../../components/img/img';
+
+interface ICardProps {
+ textName: string,
+ src: string,
+ Descriptio : string,
+ Use: string,
+}
+
+const styleContainerPlugin = {
+ padding: '15px 15px'
+ 
+};
+
+const styleContainerCard = {
+ display:'flex', 
+ flexWrap: 'wrap', 
+ gap: '10px',
+ padding: '10px'
+ 
+};
+
+const ListPlugins: ICardProps[] = [
+ {
+  textName: "calculator",
+  src: 'javaScript.png',
+  Descriptio : 'string',
+  Uso: 'string',
+ },
+ {
+  textName: "excel",
+  src: 'javaScript.png',
+  Descriptio : 'string',
+  Uso: 'string',
+ },
+ {
+  textName: "word",
+  src: 'javaScript.png',
+  Descriptio : 'string',
+  Uso: 'string',
+ },
+ {
+  textName: "temaNigth",
+  src: 'react.png',
+  Descriptio : 'string',
+  Uso: 'string',
+ },
+ {
+  textName: "calculator",
+  src: 'javaScript.png',
+  Descriptio : 'string',
+  Uso: 'string',
+ },
+ {
+  textName: "excel",
+  src: 'javaScript.png',
+  Descriptio : 'string',
+  Uso: 'string',
+ },
+ {
+  textName: "word",
+  src: 'javaScript.png',
+  Descriptio : 'string',
+  Uso: 'string',
+ },
+ {
+  textName: "temaNigth",
+  src: 'react.png',
+  Descriptio : 'string',
+  Uso: 'string',
+ },
+ {
+  textName: "calculator",
+  src: 'javaScript.png',
+  Descriptio : 'string',
+  Uso: 'string',
+ },
+ {
+  textName: "excel",
+  src: 'javaScript.png',
+  Descriptio : 'string',
+  Uso: 'string',
+ },
+ {
+  textName: "word",
+  src: 'javaScript.png',
+  Descriptio : 'string',
+  Uso: 'string',
+ },
+ {
+  textName: "temaNigth",
+  src: 'react.png',
+  Descriptio : 'string',
+  Uso: 'string',
+ },
+ {
+  textName: "font family arial",
+  src: 'react.png',
+  Descriptio : 'string',
+  Uso: 'string',
+ }
+]
+
 export default function PluginNew() : JSX.Element {
   return (
-    <div>
-      <h2>PluginNew</h2>
+    <div style={styleContainerPlugin} >
+      <h1 style={{
+      textAlign:'center',
+      margin: '5px 5px', 
+      fontSize: '50px',
+      fontWeight: 'bold',
+       
+      }} >Plugins</h1>
+      <div style={styleContainerCard} >
+        { ListPlugins.map((e,i)=> <Card textName={e.textName} src={e.src} Descriptio={e.Descriptio} use={e.use} /> ) }
+      </div>
     </div>
-  );
+  )
 };
+
+const styleTextName = {
+ display:'block',
+ textAlign:'center',
+ width:'100%'
+ 
+};
+
+const styleCard = { 
+ border:'1px solid rgba(98,157,208,0.1)', 
+ borderRadius:'10px 10px', 
+ boxShadow: '0 0 2px 0px rgba(98,157,208,1)',
+ padding:'5px 5px',
+ display:'inline-block',
+ backgroundColor:'rgb(255,255,255)',
+ backdropFilter: 'blur(10px)',
+ width: '120px',
+ 
+};
+ 
+const styleImga = {
+ width:'100px', 
+ heigth:'100px', 
+ display:'block', 
+ margin:'auto'
+ 
+};
+
+function Card(props: ICardProps) {
+ return (
+  <button type='button' style={styleCard} onClick={(event:any)=> 
+    { 
+     alert('click')
+      navigate('/' || "", { state: props  })
+    }} >
+    <img style={styleImga} src={props.src} alt="Image del plugin" />
+    <label style={styleTextName} >{props.textName}</label>
+  </button>
+
+//     const location = useLocation();
+//     const _obj = location.state as ISkill;
+
+ )
+}
